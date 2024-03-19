f07e77870021f4cf07549615a4f69b0c9def7cbf
ankitKumar <20bcs6801@cuchd.in>
2024-03-19 22:33:20 +0530

    first commit

---
+++ models/product.model.js
@@ -0,0 +1,31 @@
+const { Timestamp } = require("mongodb");
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide product name"],
+    },
+    quantity: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+module.exports = Product;
